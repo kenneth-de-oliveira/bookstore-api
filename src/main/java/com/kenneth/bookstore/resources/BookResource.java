b4947a5261095e81e553894cc128a3f153e91b5a
kennetholiv <kennetholiveira2015@gmail.com>
2021-02-19 14:51:35 -0300

    implementação do findAll, buscando todos os livros de uma determinada categoria.

--- src/main/java/com/kenneth/bookstore/resources/BookResource.java
+++ src/main/java/com/kenneth/bookstore/resources/BookResource.java
@@ -1,13 +1,18 @@
 package com.kenneth.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kenneth.bookstore.domain.Book;
+import com.kenneth.bookstore.dtos.BookDTO;
 import com.kenneth.bookstore.services.BookService;
 
 @RestController
@@ -22,4 +27,14 @@ public class BookResource {
 		Book obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@GetMapping
+	public ResponseEntity<List<BookDTO>> findAll(
+			@RequestParam(value = "category", defaultValue = "0") final Integer id) {
+		// localhost:8080/book?category=1
+		List<Book> list = service.findAll(id);
+		List<BookDTO> listDTO = list.stream().map(obj -> new BookDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
 }
--- src/main/java/com/kenneth/bookstore/services/BookService.java
+++ src/main/java/com/kenneth/bookstore/services/BookService.java
@@ -1,5 +1,6 @@
 package com.kenneth.bookstore.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,18 @@ public class BookService {
 	@Autowired
 	private BookRepository bookRepository;
 
+	@Autowired
+	private CategoryService categoryService;
+
 	public Book findById(final Integer id) {
 		Optional<Book> obj = bookRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjetoNotFoundException(
 				"the object was not found, id:" + id + ", type: " + ObjetoNotFoundException.class.getName()));
+	}
+
+	public List<Book> findAll(final Integer id) {
+		categoryService.findById(id);
+		return bookRepository.findAllByCategory(id);
 	}
 
 }
