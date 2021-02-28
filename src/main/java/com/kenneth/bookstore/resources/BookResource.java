139208173e2d8b477ca884f2e555a8087dd0100c
kennetholiv <kennetholiveira2015@gmail.com>
2021-02-28 17:44:58 -0300

    implementação do método updatePatch de livros

--- src/main/java/com/kenneth/bookstore/resources/BookResource.java
+++ src/main/java/com/kenneth/bookstore/resources/BookResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,6 +42,12 @@ public class BookResource {
 	
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Book> update(@PathVariable final Integer id, @RequestBody Book newBook){
+		Book obj = service.update(id, newBook);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Book> updatePatch(@PathVariable final Integer id, @RequestBody Book newBook){
 		Book obj = service.update(id, newBook);
 		return ResponseEntity.ok().body(obj);
 	}
