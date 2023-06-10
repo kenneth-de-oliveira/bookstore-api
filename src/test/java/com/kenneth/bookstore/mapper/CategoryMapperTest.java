da5f29e33741fd02bb66caa7df16bc1f7cc137eb
Luiz Henrique Cavalcanti <luizhsc.araujo@outlook.com>
2023-06-10 00:40:36 -0300

    test: criação de testes do CategoryMapper

--- src/test/java/com/kenneth/bookstore/mapper/CategoryMapperTest.java
+++ src/test/java/com/kenneth/bookstore/mapper/CategoryMapperTest.java
@@ -1,5 +1,8 @@
 package com.kenneth.bookstore.mapper;
 
+import com.kenneth.bookstore.dto.CategoryDTO;
+import com.kenneth.bookstore.entity.Category;
+import com.kenneth.bookstore.factory.BookStoreApiFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,21 +15,30 @@ class CategoryMapperTest {
     void mapper_whenToGetCategoryEntity_returnsCategoryDTO() {
 
         // Arrange
-
+        var fake  = BookStoreApiFactory.getCategoryList().stream().findFirst().get();
         // Act
-
+        CategoryDTO categoryDTO = CategoryMapper.mapperToDTO(fake);
         // Assert
-
+        assertEquals(fake.getName(),categoryDTO.getName());
+        assertEquals(fake.getId(),categoryDTO.getId());
     }
 
     @Test
     void mapper_whenToGetCategoryDTO_returnsCategoryEntity() {
 
         // Arrange
-
+        var fake  = BookStoreApiFactory.getCategoryList().stream().findFirst().get();
         // Act
+        var categoryDTO = new CategoryDTO();
+        categoryDTO.setId(fake.getId());
+        categoryDTO.setName(fake.getName());
+        categoryDTO.setDescription(fake.getDescription());
 
+        Category category = CategoryMapper.mapperToEntity(categoryDTO);
         // Assert
+        assertEquals(category.getId(),categoryDTO.getId());
+        assertEquals(category.getName(),categoryDTO.getName());
+        assertEquals(category.getDescription(),categoryDTO.getDescription());
 
     }
 
